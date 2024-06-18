dfbadcceebdf63688e680d8f6cbef6abfe0a689f
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2024-06-19 03:59:46 +0600

    splashscreen, partially login screen updated to compose

--- app/src/main/java/com/akash/nou/view/activity/AuthActivity.kt
+++ app/src/main/java/com/akash/nou/view/activity/AuthActivity.kt
@@ -5,10 +5,33 @@
 
 package com.akash.nou.view.activity
 
+import Constant
 import GenericApiResponse
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.akash.nou.R
 import com.akash.nou.databinding.ActivityAuthBinding
@@ -30,17 +53,19 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
 
+        setContent {
+            AuthActivityContent()
+        }
+
 
         /**
          * Homepage
          */
-        if (sharedPref.getString(applicationContext, "authToken") != "")
-            startActivity(
-                Intent(
-                    this@AuthActivity,
-                    HomepageActivity::class.java
-                ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            )
+        if (sharedPref.getString(applicationContext, "authToken") != "") startActivity(
+            Intent(
+                this@AuthActivity, HomepageActivity::class.java
+            ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        )
 
 
         /**
@@ -58,17 +83,14 @@ class AuthActivity : AppCompatActivity() {
          */
         binding.loginBtn.setOnClickListener {
             // Valid Phone Number
-            if (binding.phoneNo.text.length == 11 && binding.phoneNo.text.startsWith("01"))
-                authViewModel.verifyPhoneNumber(binding.phoneNo.text.toString())
+            if (binding.phoneNo.text.length == 11 && binding.phoneNo.text.startsWith("01")) authViewModel.verifyPhoneNumber(
+                binding.phoneNo.text.toString()
+            )
 
             // Invalid Phone Number
-            else
-                showTopToast(
-                    applicationContext,
-                    "১১ ডিজিটের একটি বৈধ নম্বর দিন",
-                    "short",
-                    "negative"
-                )
+            else showTopToast(
+                applicationContext, "১১ ডিজিটের একটি বৈধ নম্বর দিন", "short", "negative"
+            )
         }
 
 
@@ -82,40 +104,101 @@ class AuthActivity : AppCompatActivity() {
                     if (resultData.status == "Success") {
                         startActivity(
                             Intent(
-                                this@AuthActivity,
-                                OTPActivity::class.java
+                                this@AuthActivity, OTPActivity::class.java
                             ).putExtra("phone", binding.phoneNo.text.toString())
                         )
-                    } else {
+                    }
+                    else {
                         binding.phoneNo.error = "দুঃখিত! আপনি নিবন্ধিত ব্যবহারকারী না"
                     }
                 }
 
                 is GenericApiResponse.Error -> {
                     showTopToast(
-                        applicationContext,
-                        "দুঃখিত! কারিগরি ত্রুটি হয়েছে ☹️",
-                        "short",
-                        "neutral"
+                        applicationContext, "দুঃখিত! কারিগরি ত্রুটি হয়েছে ☹️", "short", "neutral"
                     )
                 }
 
                 else -> {
                     showTopToast(
-                        applicationContext,
-                        "দুঃখিত! অজানা ত্রুটি হয়েছে ☹️",
-                        "short",
-                        "neutral"
+                        applicationContext, "দুঃখিত! অজানা ত্রুটি হয়েছে ☹️", "short", "neutral"
                     )
                 }
             }
         }
 
         authViewModel.isLoading.observe(this@AuthActivity) { isLoading ->
-            if (isLoading)
-                loadingDialog.startLoading()
-            else
-                loadingDialog.dismissLoading()
+            if (isLoading) loadingDialog.startLoading()
+            else loadingDialog.dismissLoading()
         }
+    }
+
+    @Composable
+    fun AuthActivityContent() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 24.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.nou_icon),
+                    contentDescription = "Logo " +
+                            "Here",
+                    modifier = Modifier.size(128.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "লগইন করুন",
+                    style = TextStyle(
+                        fontFamily = Constant().balooda2font,
+                        fontSize = 40.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Left
+                    ),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+                Text(
+                    text = "নৌযানের টিকেট ব্যবস্থাপনার এক বিশ্বস্ত নাম",
+                    style = TextStyle(
+                        fontFamily = Constant().balooda2font,
+                        fontSize = 14.sp,
+                        color = Constant().light_gray_2,
+                        textAlign = TextAlign.Left
+                    ),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+                Text(
+                    modifier = Modifier.padding(top = 16.dp),
+                    text = "ফোন নম্বর",
+                    style = TextStyle(
+                        fontFamily = Constant().balooda2font,
+                        fontSize = 14.sp,
+                        color = Constant().light_gray_2,
+                        textAlign = TextAlign.Left
+                    ),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+            }
+        }
+
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        AuthActivityContent()
     }
 }
--- app/src/main/java/com/akash/nou/view/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/akash/nou/view/activity/SplashScreenActivity.kt
@@ -10,21 +10,55 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.akash.nou.R
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material.Text
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
-        setContentView(R.layout.activity_splash_screen)
+
+        setContent {
+            SplashScreenContent()
+        }
 
         Handler().postDelayed({
-            this@SplashScreenActivity.startActivity(
-                Intent(
-                    this@SplashScreenActivity,
-                    AuthActivity::class.java
-                )
-            )
+            startActivity(Intent(this, AuthActivity::class.java))
             finish()
         }, 1200)
     }
+}
+
+@Composable
+fun SplashScreenContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.nou_icon), // Replace with your image
+            contentDescription = "Splash Image",
+            modifier = Modifier.size(128.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Preview(showBackground = true)@Composable
+fun DefaultPreview() {
+    SplashScreenContent()
 }
